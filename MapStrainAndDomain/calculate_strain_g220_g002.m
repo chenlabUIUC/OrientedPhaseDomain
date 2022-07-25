6a791409f9916d41dc6b75c8a44fffd2c94313b9
chenlabUIUC <61767704+chenlabUIUC@users.noreply.github.com>
2022-07-25 12:51:35 -0500

    Add files via upload

--- MapStrainAndDomain/calculate_strain_g220_g002.m
+++ MapStrainAndDomain/calculate_strain_g220_g002.m
@@ -1,11 +1,13 @@
 % NAME:  Calculate_strain_(g220)_(g002)
 % PURPOSE:  This script is designed to calculate the local strain for
-% cathode NPs along x_(g220) and y_(g002) directions
+% cathode NPs along x_(g220) and y_(g002) directions based on reciprocal
+% lattice vectors derived from 4D-STEM data
 % INPUT:
 %           g vector matrix processed from 4D-STEM data: '(g002).xlsx' and '(g220).xlsx'
 % OUTPUT:
 %           Strain matrix and tetragonality matrix: 'strain_(g002).xlsx', 'strain_(g220).xlsx', 'ratiobetween(g002)and(g220).xlsx'
 % HISTORY:  written by Wenxiang Chen, 2019
+
 
 % Input data
 filename2= '(g002).xlsx'; % file with the absolute value of g002 in reciprocal space in the unit nm-1
