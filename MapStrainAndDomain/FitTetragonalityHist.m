59d930d5cb4efb8193a58dea4d9dd8ac5117b53f
chenlabUIUC <61767704+chenlabUIUC@users.noreply.github.com>
2022-07-26 10:40:08 -0500

    Add files via upload

--- MapStrainAndDomain/FitTetragonalityHist.m
+++ MapStrainAndDomain/FitTetragonalityHist.m
@@ -15,11 +15,13 @@ pos_y=B(:,2);
 
 % An example of the fitting of the histogram is provided here:
 [result, good] = createFit(pos_x, pos_y);
+% Check the fitting results
 result.b2  % threshold to distinguish the [100]t and pesudo cubic phase
-result.b4  % threshold to distinguish the [111]t and pesudo cubic phase
+result.b4  % threshold to distinguish the [111]t and pesudo cubic phase. 
 
 % Note: One can perform fitting for cathode particles using the cftool as follows.
-%       Fitting was performed with some constraints, considering the theoretical 
-%       values of tetragonality. Please also refer to our manuscript.
+%       Fitting was performed with constraints, considering the theoretical 
+%       values of tetragonality: R=0.613 for [100]t, R=0.707 for [110]c, R=0.756 
+%       for [111]t. Please also refer to our manuscript.
 
 % cftool(pos_x,pos_y);  % curve fitting tool
